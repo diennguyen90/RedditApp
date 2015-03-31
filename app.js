f9b9b9d085fb367df21c0eda2040fce5b3fe4504
diennguyen90 <diennguyen90@yahoo.com>
2015-03-31 15:14:54 -0700

    minor changes

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
-angular.module('redditApp', [])
+angular.module('redditApp', ['ui.router'])
 
-.controller('MainCtrl', function($scope){
-	$scope.posts =['post1','post2','post3','post4']
-})
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	$stateProvider
+	.state('home', {
+		url: '/home',
+		templateUrl: '/home.html',
+		controller: 'MainCtrl'
+	})
+	.state('posts', {
+		url:'/posts/{id}',
+		templateUrl: '/post.html',
+		controller: 'PostsCtrl'
+	});
+	$urlRouterProvider.otherwise('home');
+}])
+
+
+.controller('MainCtrl', ['$scope', function($scope){
+	// $scope.posts =[
+	// {title:'post1', upvotes : 5},
+	// {title:'post2', upvotes : 7},
+	// {title:'post3', upvotes: 10},
+	// {title:'post4', upvotes : 15}
+	// ];
+	$scope.posts = []
+
+	$scope.addPost = function(){
+		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes : 0})
+		$scope.title = '';
+		$scope.link = '';
+		if($scope.title === '') return false;
+}
+	//adds up 1 vote every click
+	$scope.addUpvotes = function(post){
+		post.upvotes += 1;
+	}
+
+}]);
+
+// .controller('Posts', ['$scope', '$stateParams' function($scope,$stateParams){
+
+// }])
